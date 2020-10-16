c87af6f4dfc8aca287ae4da20d0779ef7297c784
ifiri <raymondcostner@gmail.com>
2020-10-17 00:11:45 +0300

    feat: add additional icons

--- src/components/common/Icon/Icon.constants.js
+++ src/components/common/Icon/Icon.constants.js
@@ -3,11 +3,42 @@ import { ReactComponent as AudienceIcon } from '@/assets/icons/audience.svg';
 import { ReactComponent as SmileFilledIcon } from '@/assets/icons/smile-filled.svg';
 import { ReactComponent as MarkListFilledIcon } from '@/assets/icons/mark-list-filled.svg';
 import { ReactComponent as PaletteIcon } from '@/assets/icons/palette.svg';
+import { ReactComponent as CheckIcon } from '@/assets/icons/check.svg';
+import { ReactComponent as ChevronDownIcon } from '@/assets/icons/chevron-down.svg';
+import { ReactComponent as ChevronUpIcon } from '@/assets/icons/chevron-up.svg';
+import { ReactComponent as ChevronLeftIcon } from '@/assets/icons/chevron-left.svg';
+import { ReactComponent as ChevronRightIcon } from '@/assets/icons/chevron-right.svg';
+import { ReactComponent as CloseIcon } from '@/assets/icons/close.svg';
+import { ReactComponent as CopyIcon } from '@/assets/icons/copy.svg';
+import { ReactComponent as EditIcon } from '@/assets/icons/edit.svg';
+import { ReactComponent as FilterIcon } from '@/assets/icons/filter.svg';
+import { ReactComponent as PauseIcon } from '@/assets/icons/pause.svg';
+import { ReactComponent as PlayIcon } from '@/assets/icons/play.svg';
+import { ReactComponent as PeopleIcon } from '@/assets/icons/people.svg';
+import { ReactComponent as SearchIcon } from '@/assets/icons/search.svg';
+import { ReactComponent as TagIcon } from '@/assets/icons/tag.svg';
+import { ReactComponent as TrashIcon } from '@/assets/icons/trash.svg';
+
 
 export const ICONS_MAP = {
-  chain: ChainIcon,
-  audience: AudienceIcon,
+  'chain': ChainIcon,
+  'audience': AudienceIcon,
   'smile-filled': SmileFilledIcon,
   'mark-list-filled': MarkListFilledIcon,
-  palette: PaletteIcon,
+  'palette': PaletteIcon,
+  'check': CheckIcon,
+  'chevron-down': ChevronDownIcon,
+  'chevron-up': ChevronUpIcon,
+  'chevron-left': ChevronLeftIcon,
+  'chevron-right': ChevronRightIcon,
+  'close': CloseIcon,
+  'copy': CopyIcon,
+  'edit': EditIcon,
+  'filter': FilterIcon,
+  'pause': PauseIcon,
+  'play': PlayIcon,
+  'people': PeopleIcon,
+  'search': SearchIcon,
+  'tag': TagIcon,
+  'trash': TrashIcon,
 };
